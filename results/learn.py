b5bd8587374afb73607f16dba4b5face199e16cb
Daniel Lemire <lemire@gmail.com>
2018-06-19 12:19:16 +0200

    Plots.

--- results/learn.py
+++ results/learn.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -21,4 +22,49 @@ for file in ["skylake/vecbench.txt", "skylake/hashbench.txt", "armA57/vecbench.t
   df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
   print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
   print('relative Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred)/y_test.mean())
+
+  #dataset.plot(x='Hours', y='Scores', style='o')
+  #plt.title('Hours vs Percentage')
+  #plt.xlabel('Hours Studied')
+  #plt.ylabel('Percentage Score')
+  f = plt.figure()
+  plt.scatter(y_test, y_pred)
+  plt.scatter(y_test, y_test)
+
+  plt.ylabel('predicted running time')
+  plt.xlabel('actual running time')
+  filename = file+".pdf"
+  if(os.path.exists(filename)):
+    os.remove(filename)
+  f.savefig(filename, bbox_inches='tight')
+  print(filename)
+  print()
+
+
+for file in ["skylake/vecbench.txt", "skylake/hashbench.txt", "armA57/vecbench.txt"]:
+  print("file: ", file)
+  dataset = pd.read_csv(file, delimiter=" ")
+  X=dataset.iloc[:,2:4]
+  y=dataset.iloc[:,4]
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+  regressor = LinearRegression()
+  regressor.fit(X_train, y_train)
+
+  print("intercept: ", regressor.intercept_, " coefs ", regressor.coef_)
+
+  y_pred = regressor.predict(X_test)
+  df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+  print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+  print('relative Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred)/y_test.mean())
+  f = plt.figure()
+  plt.scatter(y_test, y_pred)
+  plt.ylabel('predicted running time')
+  plt.xlabel('actual running time')
+  filename = file+"NoMax.pdf"
+  if(os.path.exists(filename)):
+    os.remove(filename)
+  f.savefig(filename, bbox_inches='tight')
+  print(filename)
+  print()
   print()
